7372bbdf208fcf4804529d4fd33a762b9e085e7d
EmmTarSystem <manu974.training@gmail.com>
2024-11-02 00:04:54 +0100

    0.0.1

---
+++ scripts/itemSystem.js
@@ -0,0 +1,20 @@
+
+// clique sur un item
+
+function onClickOnItem(params) {
+    document.getElementById("divItemEditor").style.display ="block";
+}
+
+
+
+
+
+// Ferme l'editeur d'item
+
+function onCloseItemEditor() {
+    document.getElementById("divItemEditor").style.display ="none";
+}
+
+
+
+
