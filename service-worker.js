4bd06f6b22bc34c0f2d383f247ea13ac735e6cb8
EmmTarSystem <manu974.training@gmail.com>
2024-11-24 11:38:46 +0100

    1.1.8 new service worker

--- service-worker.js
+++ service-worker.js
@@ -1,102 +1,60 @@
-let pwa_cache_version = "MSS-cache-1.1.7";//version du dernier cache
 
+const PREFIX = "V1";//Numero de version
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open(pwa_cache_version).then((cache) => {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'manifest.json',
-          'Icons/Icon-Accepter.png', 
-          'Icons/Icon-Delete.png', 
-          'Icons/Icon-Download.png', 
-          'Icons/Icon-Favoris-Sel.png', 
-          'Icons/Icon-Favoris.png', 
-          'Icons/Icon-Info.png', 
-          'Icons/Icon-New.png', 
-          'Icons/Icon-Profil.png', 
-          'Icons/Icon-Return-cancel.png', 
-          'Icons/Icon-Setting.png', 
-          'Icons/Icon-Upload.png', 
-          'Icons/Icon-Valider.png', 
-          'Icons/Logo_MSS-192.png', 
-          'Icons/Logo_MSS-512.png', 
-          'Icons/MSS-Logo.ico',
-          'images/icon-art-martiaux.png', 
-          'images/icon-autre-divers.png', 
-          'images/icon-badminton.png', 
-          'images/icon-baseball.png', 
-          'images/icon-basketball.png', 
-          'images/icon-boxe.png', 
-          'images/icon-breakdance.png', 
-          'images/icon-cap.png', 
-          'images/icon-crossfit.png', 
-          'images/icon-danse.png', 
-          'images/icon-equitation.png', 
-          'images/icon-escalade.png', 
-          'images/icon-football.png', 
-          'images/icon-golf.png', 
-          'images/icon-handball.png', 
-          'images/icon-intense-running.png', 
-          'images/icon-marche.png', 
-          'images/icon-musculation.png', 
-          'images/icon-natation.png', 
-          'images/icon-nautique.png', 
-          'images/icon-patin.png', 
-          'images/icon-rugby.png', 
-          'images/icon-ski.png', 
-          'images/icon-snowboard.png', 
-          'images/icon-sport-co.png', 
-          'images/icon-stretching.png', 
-          'images/icon-tennis-de-table.png', 
-          'images/icon-tennis.png', 
-          'images/icon-triathlon.png', 
-          'images/icon-velo.png', 
-          'images/icon-volley.png', 
-          'images/icon-yoga.png', 
-          'scripts/activitySystem.js', 
-          'scripts/app.js', 
-          'scripts/favoris.js', 
-          'scripts/importExport.js', 
-          'scripts/info.js', 
-          'scripts/notify.js', 
-          'scripts/profil.js', 
-          'scripts/setting.js', 
-          'scripts/sortAndFilter.js', 
-          'styles/global.css'
-
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
-
-
-
-
-
-  // Gestion de l'activation du Service Worker et nettoyage des anciens caches
-self.addEventListener('activate', (event) => {
-  let cacheWhitelist = [pwa_cache_version];
-
+self.addEventListener('install',(event)=>{
+  self.skipWaiting();//permet le remplacement du service worker dès que le nouveau existe
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
+    (async () => {
+      const cache = await caches.open(PREFIX);
+      cache.add(new Request('/offline.html'));
+    })()
+  );
+  console.log(`Service worker mode :  ${PREFIX} Install`);
+});
+
+
+self.addEventListener('activate', (event) => {
+  clients.claim();// Permet de controler tout de suite la page
+
+  // Suppression des anciennes clé de caches
+  event.waitUntil(
+    (async()=>{
+      const keys = await caches.keys();
+      await Promise.all(
+        keys.map((key) =>{
+          if (!key.includes(PREFIX)) {
+            return caches.delete(key);
           }
         })
       );
-    })
+    })()
   );
+
+
+  console.log(`Service worker mode : ${PREFIX} Active`)
+});
+
+
+
+
+self.addEventListener('fetch', (event) => {
+  console.log(`Service worker mode : ${PREFIX} Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
+  if (event.request.mode === 'navigate'){
+    event.respondWith(
+      (async () => {
+
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse){
+            return preloadResponse;
+          }
+  
+          return await fetch(event.request);
+        } catch (e) {
+          const cache = await caches.open(PREFIX);
+          return await cache.match('/offline.html');
+        }
+      })()
+    );
+  }
 });
