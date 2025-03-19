ed72cc126e28dc6a32ade15251adcc3921dd607c
EmmTarSystem <manu974.training@gmail.com>
2025-03-19 20:17:15 +0100

    debut d'insertion génération de session

--- scripts/session.js
+++ scripts/session.js
@@ -1117,6 +1117,193 @@ function onCloseFakeSelectSession(event) {
 }
 
 
+
+
+
+
+
+// ---------------------------- Génération de session ---------------------------------
+
+// tout est basé sur maxcounter
+
+
+
+
+
+
+function onClickMenuCreateSession() {    
+    onGenerateSessionTable();
+
+    // onGenerateModelSelectList(); A activer lorsque j'aurai les modèles
+}
+
+// Classe d'une ligne de session
+class TableLineSession{
+
+    constructor(parentRef,idNumber){
+        this.parentRef = parentRef;
+        this.idNumber = idNumber;
+
+        // la row
+        this.element = document.createElement("tr");
+        this.render();
+    }
+
+    render(){
+        this.element.innerHTML = `
+            <td class="gen-session-col-nom">
+                <input type="text" id="inputGenSessionNom_${this.idNumber}" class="gen-session-col-nom" placeholder="Compteur ${this.idNumber}">
+            </td>
+            <td class="gen-session-col-series">
+                <input type="number" id="inputGenSessionSerie_${this.idNumber}" class="gen-session-col-series" placeholder="0">
+            </td>
+            <td class="gen-session-col-rep">
+                <input type="number" id="inputGenSessionRep_${this.idNumber}" class="gen-session-col-rep" placeholder="0">
+            </td>
+            <td class="gen-session-col-color"  id="tdGenSessionChooseColor_${this.idNumber}">
+                <select id="selectGenSessionColor_${this.idNumber}" onchange="onChangeColorInGenSessionTable(${this.idNumber})">
+                    <option value="white">Blanc</option>
+                    <option value="green">Vert</option>
+                    <option value="yellow">Jaune</option>
+                    <option value="red">Rouge</option>
+                    <option value="blue">Bleu</option>
+                    <option value="violet">Violet</option>
+                </select>
+            </td>
+        `;
+
+
+        // Insertion
+        this.parentRef.appendChild(this.element);
+    }
+
+}
+
+
+
+
+
+
+// Génération du tableau de création de session
+function onGenerateSessionTable() {
+    
+
+
+    // Reférence le parent
+    let parentRef = document.getElementById("bodyTableGenerateSession");
+
+    // Reset le contenu du parent
+    parentRef.innerHTML = "";
+
+    // Génère le tableau
+    for (let i = 0; i < maxCounter; i++) {
+        new TableLineSession(parentRef,i); 
+    }
+
+    // Affiche le popup
+    document.getElementById("divPopCreateSession").style.display = "flex";
+}
+
+
+
+
+// Génération de la session
+
+function onGenerateSession() {
+    let sessionList = [];
+
+    for (let i = 0; i < maxCounter; i++) {
+
+        // Reférence les éléments
+        inputName = document.getElementById(`inputGenSessionNom_${i}`);
+        inputSerie = document.getElementById(`inputGenSessionSerie_${i}`);
+        inputRep = document.getElementById(`inputGenSessionRep_${i}`);
+        selectColor = document.getElementById(`selectGenSessionColor_${i}`);
+
+        // Si inputName remplit
+        if (inputName.value != "") {
+
+            // récupère les éléments de la ligne 
+            sessionList.push( {
+                name: inputName.value, 
+                serieTarget: inputSerie.value || 0,
+                repIncrement:inputRep.value || 0,
+                color : selectColor.value
+            })
+        } 
+    }
+
+    console.log(sessionList);
+}
+
+
+
+// changement de couleur dans le tableau de génération
+function onChangeColorInGenSessionTable(idRef) {
+    let tableDataRef = document.getElementById(`tdGenSessionChooseColor_${idRef}`),
+    colorRef = document.getElementById(`selectGenSessionColor_${idRef}`).value;
+    tableDataRef.style.backgroundColor = counterColor[colorRef];
+}
+
+
+
+// Génération des options du selecteur de session
+function onGenerateModelSelectList(params) {
+    // Referencement
+    let parentRef = document.getElementById("selectSessionTableModelName");
+    
+    // Vide les enfants
+    parentRef.innerHTML = "";
+
+    // Insert l'option "Personnalisé"
+    let defaultOption = document.createElement("option");
+        defaultOption.value = "CUSTOM";
+        defaultOption.innerHTML = "Personnalisé";
+
+    parentRef.appendChild(defaultOption);
+
+    // Pour chaque nom de model
+    counterModelName.forEach(e=>{
+
+        // crée une option et l'insere
+        let newOption = document.createElement("option");
+        newOption.value = e;
+        newOption.innerHTML = e;
+
+        parentRef.appendChild(newOption);
+    });
+}
+
+
+// Fonction pour empecher la div de se ferme lorsqu'on se trouve dans sa zone.
+function onClickOnCreateSessionArea(event){
+    event.stopPropagation();
+}
+
+
+// Annulation de la création de session
+function onCancelCreateSession(event) {
+
+    // masque le popup
+    document.getElementById("divPopCreateSession").style.display = "none";
+
+    //vide le tableau
+    document.getElementById("selectSessionTableModelName").innerHTML = "";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // Retour depuis Info
 function onClickReturnFromSession() {
 
